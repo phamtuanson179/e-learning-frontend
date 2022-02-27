45d98a407b32851323e14f7c16aec538f275430a
ngasocute <nguyennga1606ictbk@gmail.com>
2022-02-27 23:17:21 +0700

    test

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
 
 // react-router-dom components
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 // @mui material components
 import Card from "@mui/material/Card";
 import Switch from "@mui/material/Switch";
 import Grid from "@mui/material/Grid";
-import MuiLink from "@mui/material/Link";
+//import MuiLink from "@mui/material/Link";
 
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import GoogleIcon from "@mui/icons-material/Google";
+// // @mui icons
+// import FacebookIcon from "@mui/icons-material/Facebook";
+// import GitHubIcon from "@mui/icons-material/GitHub";
+// import GoogleIcon from "@mui/icons-material/Google";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
@@ -29,12 +30,41 @@ import routes from "routes";
 
 // Images
 import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+// import axios from "./api/loginApi.js";
+// import AuthContext from "/AuthContext.js";
+// const LOGIN_URL='http://0.0.0.0:8000/login  ';
 
-function SignInBasic() {
+const SignInBasic = () => {
   const [rememberMe, setRememberMe] = useState(false);
 
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useNavigate();
+
+  useEffect(()=> {
+    if (localStorage.getItem('user-info')){
+      history.push("/add")
+    }
+  },[])
+  async function Login () {
+    console.warn(email, password)
+    let item = (email, password);
+    let result = await fetch("http://0.0.0.0:8000/login",{
+      method: 'POST',
+      headers:{
+        "content-type":"application/json",
+        "accept":"application/json"
+      },
+      body: JSON.stringify(item)
+    });
+    result = await result.json();
+    localStorage.setItem(JSON.stringify(result))
+    history.push('./add')
+
+  }
+ 
   return (
     <>
       <MKBox
@@ -92,54 +122,26 @@ function SignInBasic() {
                   color='white'
                   mt={1}
                 >
-                  Sign in
+                  Đăng nhập
                 </MKTypography>
-                <Grid
-                  container
-                  spacing={3}
-                  justifyContent='center'
-                  sx={{ mt: 1, mb: 2 }}
-                >
-                  <Grid item xs={2}>
-                    <MKTypography
-                      component={MuiLink}
-                      href='#'
-                      variant='body1'
-                      color='white'
-                    >
-                      <FacebookIcon color='inherit' />
-                    </MKTypography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <MKTypography
-                      component={MuiLink}
-                      href='#'
-                      variant='body1'
-                      color='white'
-                    >
-                      <GitHubIcon color='inherit' />
-                    </MKTypography>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <MKTypography
-                      component={MuiLink}
-                      href='#'
-                      variant='body1'
-                      color='white'
-                    >
-                      <GoogleIcon color='inherit' />
-                    </MKTypography>
-                  </Grid>
-                </Grid>
               </MKBox>
               <MKBox pt={4} pb={3} px={3}>
                 <MKBox component='form' role='form'>
                   <MKBox mb={2}>
-                    <MKInput type='email' label='Email' fullWidth />
+                    <MKInput 
+                      type='email' 
+                      label='Email' 
+                      onChange={(e)=>setEmail(e.target.value)}
+                      fullWidth />
                   </MKBox>
                   <MKBox mb={2}>
-                    <MKInput type='password' label='Password' fullWidth />
+                    <MKInput 
+                      type='password' 
+                      label='Password' 
+                      onChange={(e)=>setPassword(e.target.value)}
+                      fullWidth />
                   </MKBox>
+
                   <MKBox display='flex' alignItems='center' ml={-1}>
                     <Switch
                       checked={rememberMe}
@@ -152,28 +154,23 @@ function SignInBasic() {
                       onClick={handleSetRememberMe}
                       sx={{ cursor: "pointer", userSelect: "none", ml: -1 }}
                     >
-                      &nbsp;&nbsp;Remember me
+                      Lưu mật khẩu
                     </MKTypography>
                   </MKBox>
+
                   <MKBox mt={4} mb={1}>
-                    <MKButton variant='gradient' color='info' fullWidth>
-                      sign in
+                    <MKButton 
+                    variant='gradient' 
+                    color='info' 
+                    onClick={Login}
+                    fullWidth>
+                      Đăng nhập
                     </MKButton>
                   </MKBox>
                   <MKBox mt={3} mb={1} textAlign='center'>
-                    <MKTypography variant='button' color='text'>
-                      Don&apos;t have an account?{" "}
-                      <MKTypography
-                        component={Link}
-                        to='/authentication/sign-up/cover'
-                        variant='button'
-                        color='info'
-                        fontWeight='medium'
-                        textGradient
-                      >
-                        Sign up
-                      </MKTypography>
-                    </MKTypography>
+                    <MKButton variant='button' color='text'>
+                      Bạn quên mật khẩu?
+                    </MKButton> 
                   </MKBox>
                 </MKBox>
               </MKBox>
