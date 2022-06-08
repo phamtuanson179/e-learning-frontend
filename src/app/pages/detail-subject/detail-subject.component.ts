e86dbad59ea2f456b7548139878fe89cdc1091e5
Phạm Tuấn Sơn <phamtuanson2000@gmail.com>
2022-06-08 23:32:02 +0700

    feat: add api json-server

---
+++ src/app/pages/detail-subject/detail-subject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-subject',
+  templateUrl: './detail-subject.component.html',
+  styleUrls: ['./detail-subject.component.scss']
+})
+export class DetailSubjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/subject/subject.component.ts
+++ src/app/pages/subject/subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subject',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    
+  }
+
+  on_click_subject(){
+    console.log('abc')
+    this.router.navigate(['subject/detail'])
   }
 
 }
